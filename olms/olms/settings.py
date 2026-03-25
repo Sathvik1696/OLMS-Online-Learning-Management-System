5f4215112453497db0aefa14fc14fccb396c1c37
Sathvik1696 <Sathvik1696@users.noreply.github.com>
2026-03-25 21:17:02 +0530

    Prepare for deployment: add GitHub Action, Procfile, WhiteNoise, and production settings

--- olms/olms/settings.py
+++ olms/olms/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-3=7_f@9^d(5^^czavkk!w_rbt1ate0z&y11n^dtufb)itl0rd$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # In production, replace '*' with your actual domain
 
 
 # Application definition
@@ -50,6 +52,7 @@ AUTH_USER_MODEL ='accounts.user'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,14 +85,9 @@ WSGI_APPLICATION = 'olms.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'olms',
-        'USER': 'postgres',
-        'PASSWORD': 'Sathvik@1696',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:Sathvik@1696@localhost:5432/olms'
+    )
 }
 
 
@@ -128,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
